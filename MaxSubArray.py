4dd425108e69e5df0c9142f330222e70f1988531
Jason Fung <jasonfungsing@gmail.com>
2014-03-15 10:18:07 +1100

    added find max subarray

--- MaxSubArray.py
+++ MaxSubArray.py
@@ -2,7 +2,7 @@ __author__ = 'jasonfungsing'
 
 
 def find_max_crossing_subarray(array, low, mid, high):
-    left_sum = None  # set it to minus infinite
+    left_sum = None
     sum1 = 0
     max_left = 0
     for i in range(mid, low - 1, -1):
@@ -11,13 +11,13 @@ def find_max_crossing_subarray(array, low, mid, high):
             left_sum = sum1
             max_left = i
         elif sum1 > left_sum:
-            left_sum = sum
+            left_sum = sum1
             max_left = i
 
-    right_sum = None  # set it to minus infinite
+    right_sum = None
     sum2 = 0
     max_right = 0
-    for j in range(mid + 1, high - 1):
+    for j in range(mid + 1, high + 1):
         sum2 = sum2 + array[j]
         if None == right_sum:
             right_sum = sum2
@@ -30,8 +30,22 @@ def find_max_crossing_subarray(array, low, mid, high):
 
 
 def find_max_subarray(array, low, high):
+    mid = (low + high) / 2
+    if high == low:
+        return (low, high, array[low])
+    else:
+        (left_low, left_high, left_sum) = find_max_subarray(array, low, mid)
+        (right_low, right_high, right_sum) = find_max_subarray(array, mid + 1, high)
+        (cross_low, cross_high, cross_sum) = find_max_crossing_subarray(array, low, mid, high)
+
+    if left_sum >= right_sum and left_sum >= cross_sum:
+        return (left_low, left_high, left_sum)
+    elif right_sum >= left_sum and right_sum >= cross_sum:
+        return (right_low, right_high, right_sum)
+    else:
+        return (cross_low, cross_high, cross_sum)
 
 
-
-array = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-print str(find_max_crossing_subarray(array, 0, 7, len(array)))
+# array = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+array = [-3, 2, -5, 10, -2, 8, -8, 1, 4]
+print str(find_max_subarray(array, 0, len(array)-1))
